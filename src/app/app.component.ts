3e1611f12e09e477ce12499288e39484afdb0f7d
Chabrik Abdellatif <abdoo.chbrik@gmail.com>
2022-07-07 17:55:14 +0200

    first service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CatalogueService } from './catalogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecommerce';
+export class AppComponent implements OnInit {
+
+  public categories:any;
+
+  constructor(private catService:CatalogueService) {
+
+  }
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.catService.getRessource("/categories")
+      .subscribe(data => {
+        console.log(data);
+        this.categories = data;
+      }, err => {
+        console.log(err);
+      }
+      ); 
+  }
 }
